98cc7599c4ad042a1585fe359a87cfc12f8a272a
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2021-12-27 13:31:44 +0800

    Rebuild

---
+++ digiLibrary-CLI/include/countline.hpp
@@ -0,0 +1,22 @@
+//
+//  countline.hpp
+//  Library-cli
+//
+//  Created by Rain Chen on 2021/12/15.
+//
+
+#ifndef countline_hpp
+#define countline_hpp
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <ctime>
+#include <filesystem>
+#include <unistd.h>
+using namespace std;
+//  countlines in a file
+int countline(string);
+
+#endif /* countline_hpp */
---
+++ digiLibrary-CLI/include/welcome.hpp
@@ -0,0 +1,25 @@
+//
+//  welcome.hpp
+//  Library-cli
+//
+//  Created by Rain Chen on 2021/12/13.
+//
+
+#ifndef welcome_hpp
+#define welcome_hpp
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <ctime>
+#include <filesystem>
+#include <unistd.h>
+using namespace std;
+//  welcome class
+class Welcome {
+public:
+    void you();
+};
+
+#endif /* welcome_hpp */
